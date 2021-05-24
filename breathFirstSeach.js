9310951c0504887e118e84b6f8c325b7a3891e8f
Fábio Pereira <fabiopereiragt@gmail.com>
2021-05-24 16:37:01 -0300

    fix

--- breathFirstSeach.js
+++ breathFirstSeach.js
@@ -2,13 +2,14 @@
 
 const VALUE = 0;
 const DISTANCE = 1;
+const SOURCE = 1;
 //Representando o grafo através de Listas de adjacência
 //o Grafo está no diretório grafo_img
 const graph = [
   [7, 12], //0 
   [3, 6, 10], //1 
-  [1, 4 ,8], //2 
-  [1, 2 ,7], //3 
+  [1, 4, 8], //2 
+  [1, 2, 7], //3 
   [3, 11], //4 
   [0, 2, 3], //5 
   [2, 12, 13], //6 
@@ -22,27 +23,35 @@ const graph = [
   [6, 7, 13], //14 
 ];
 
-
 let BFS = (graph, source) => {
-//    let nodeNumbers = graph.length;
   let queue = [];
-  let marked = [];  
-  let result = [];  
+  let marked = [];
+  let result = [];
   let startingDistance = 0;
   queue.push([source, startingDistance]);
   marked.push(source);
-  while(queue.length){
-    let node = queue.shift();  
+  while (queue.length) {
+    let node = queue.shift();
     node[DISTANCE] = node[DISTANCE] + 1;
-    for(let i = 0; i < graph[node[VALUE]].length; i++){
-      if(!marked.includes(graph[node[VALUE]][i])){
+    for (let i = 0; i < graph[node[VALUE]].length; i++) {
+      if (!marked.includes(graph[node[VALUE]][i])) {
         marked.push(graph[node[VALUE]][i]);
-        result.push([source, graph[node[VALUE]][i], node[DISTANCE]])    
+        result.push([graph[node[VALUE]][i], node[DISTANCE]])
+        //A fila é formada por cada nó e sua distância em relação ao ponto de partida (source)  
         queue.push([graph[node[VALUE]][i], node[DISTANCE]]);
-      }              
+      }
     }
   }
-  return result;    
+  return result;
 };
 
-console.log(BFS(graph, 1)); 
+let result = BFS(graph, SOURCE);
+
+print(result);
+
+function print(result) {
+  console.log("Distâncias:");
+  for (let i = 0; i < result.length; i++) {
+    console.log("(" + SOURCE + "," + result[i][VALUE] + "): " + result[i][DISTANCE]);
+  }
+}
